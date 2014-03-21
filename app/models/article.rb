161de9d4b1f53c42814a640f2c57d6911aa99c9f
ypw1380 <ypw1380@gmail.com>
2014-03-21 22:28:19 +0000

    another commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   has_many :article_categories
-  has_many :categories, through :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true
   validates :content, presence: true
   validates :categories, presence: true
